0475f6a7b395dd157d25f5da7a9e94029401cfd6
Lifdas <florian.potier-clemente@e-cdp.com>
2025-04-14 15:23:14 +0200

    f

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -17,6 +17,7 @@ function App() {
         headers: {}
       },
       context: {}
+      
     };
 
     // Appliquer les middlewares un par un
---
+++ front-end-react/timer.js
@@ -0,0 +1,10 @@
+export default async function timer(req, res, next) {
+    if (!req.context) req.context = {}; 
+  
+    const start = performance.now();
+    await next();
+    const end = performance.now();
+  
+    req.context.elapsed = (end - start).toFixed(1) + ' ms';
+    console.log("Timer terminé :", req.context.elapsed);
+  }
