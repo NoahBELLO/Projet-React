1a0ff0b866eccef5ba77023315855de5c5e9c5e8
Noah BELLO <noah.bello@e-cdp.com>
2025-04-18 16:48:28 +0200

    <chores>Suppression fichier non util

--- testMiddleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (req, res, next) {
-    console.log("🧠 Middleware exécuté !");
-    req.options.headers = {
-      ...req.options.headers,
-      'X-Test-Widad': 'OK'
-    };
-    next();
-  };
-  
